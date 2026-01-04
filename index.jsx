eb208f0f03c70be2ce593229c245f48e37b40c28
Zavier <rahmansyah@wisc.edu>
2026-01-04 23:37:07 +0700

    Added Route for Next Spring 2026 Semester Courses

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import App from './app';  // Here we're importing the App component from the App.jsx file
+import App from './app';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
@@ -10,7 +10,12 @@ function AppRouter() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />}/>
-        <Route path="/about" element={<AboutPage />} />
+        <Route path="/Linear Optimization" element={<LinearOptimizationPage />} />
+        <Route path="/Graphs and Networks in Data Science" element={<GraphsAndNetworksInDataSciencePage />} />
+        <Route path="/Securing Information Networks" element={<SecuringInformationNetworksPage />} />
+        <Route path="/Responsibility-Big Data and AI" element={<ResponsibilityBigDataAndAIPage />} />
+        <Route path="/Interaction Design Studio" element={<InteractionDesignStudioPage />} />
+        <Route path="/Data Storytelling With Visualization" element={<DataStorytellingWithVisualizationPage />} />
       </Routes>
     </Router>
   );
