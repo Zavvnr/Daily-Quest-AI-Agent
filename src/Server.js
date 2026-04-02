de2c5b6ac011fb8a277d82b56a17f70c6fbdb398
Zavier <rahmansyah@wisc.edu>
2026-04-01 22:07:28 -0500

    Fix Server.js for deployment

--- src/Server.js
+++ src/Server.js
@@ -178,7 +178,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // Fallback to index.html for client-side routing
-app.get('/*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
